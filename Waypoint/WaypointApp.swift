48516a69d1bf1e1be7e1f1e61860cc96c0871cd1
Kevin Perez <kp952547@gmail.com>
2025-11-11 19:05:02 -0500

    Initial Commit

---
+++ Waypoint/WaypointApp.swift
@@ -0,0 +1,32 @@
+//
+//  WaypointApp.swift
+//  Waypoint
+//
+//  Created by Kevin Perez on 11/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WaypointApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
